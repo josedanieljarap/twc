ab88262feaf96882cb3549b942ce470cc045c3b3
Daniel <josedaniel.jarap@gmail.com>
2025-07-07 21:53:38 -0500

    website structure # 2

--- utils/fonts.ts
+++ utils/fonts.ts
@@ -1,7 +1,14 @@
 import { Quicksand } from 'next/font/google'
+import { Montserrat } from 'next/font/google'
 
 export const quicksand = Quicksand({
   subsets: ['latin'],
-  weight: ['700'],
+  weight: ['700', '600', '400'],
+  display: 'swap',
+})
+
+export const montserrat = Montserrat({
+  subsets: ['latin'],
+  weight: ['700', '600', '400'],
   display: 'swap',
 })
